b60768faf8284256b6bcfd4ab17945017816adcd
egga22 <ejmbenson@gmail.com>
2025-12-10 18:13:22 -0600

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-export { TeenLifeSimulator } from './TeenLifeSimulator.js';
-export * from './systems/gradeSystem.js';
-export * from './systems/timeSystem.js';
-export * from './systems/relationshipSystem.js';
-export * from './systems/rumorSystem.js';
-export * from './systems/choresSystem.js';
-export * from './systems/sportsSystem.js';
-export * from './systems/fashionSystem.js';
